c0eed35b9e2698d503aab0905b7494f0be18a822
javersoncleidinato-afk <javersoncleidinato@gmail.com>
2026-05-28 19:46:56 -0300

    Add simple premade system for Fast coding

---
+++ Premades.py
@@ -0,0 +1,8 @@
+# simple premade system for Fast coding
+class Premade:
+    def __init__(self):
+        pass
+
+    def print(any, amount=1):
+        for i in range(amount):
+            __builtins__.print(any)
