0397523cab20cdc3367a3a789caacecd4d6e8af8
javersoncleidinato-afk <javersoncleidinato@gmail.com>
2026-05-28 21:10:48 -0300

    Improvements + wait Added on Premades-example.py

--- Premades-example.py
+++ Premades-example.py
@@ -1,5 +1,14 @@
-# adding the Premades
-pp = Premades() # Get the code on Premades.py
+# Placing Premades class
+p = Premades() # code on https://github.com/javersoncleidinato-afk/Learm/blob/main/Premades.py
 
-# 1. printing
-pp.print("Multiple in one!", 10) # prints the string 10 times
+# 1. print
+p.print("10 times!", 10)
+# prints it 10 times, if amount = None print it only 1 time
+
+# 2. wait
+print("2 seconds")
+p.wait(2)
+print("end, 1 second")
+p.wait()
+print("end")
+# wait(num), wait 'num' seconds, if None waits 1 second
